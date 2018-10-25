58b56b32aff2804de8093657c4778914fe74accd
Cassandra <cglascogueye@gmail.com>
2018-10-24 22:47:09 -0500

    Final touches to showing examples of Encap, polymor, inherit, and abstract...

--- OOPDemo/House.cs
+++ OOPDemo/House.cs
@@ -3,11 +3,23 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace OOPDemo
-{
+{   //Encapsulation:
     class House
     {
         private string bedType;
         private string toiletType;
         private decimal squareFeet;
+        public int price;
+
+        public virtual string[] HouseAmenities(string TinyHouseComforts)
+        {
+            return new string[0];
+        }
     }
+
+    
+    
+        
+
+    
 }
--- OOPDemo/Program.cs
+++ OOPDemo/Program.cs
@@ -7,41 +7,48 @@ namespace OOPDemo
         //INHERITANCE
         static void Main(string[] args)
         { }
-          class TinyHouse : House
-        { 
-            
-    }
+        class TinyHouse : House
+        {
+
+            string bedType = "murphyBed";
+            string toiletType = "compostToilet";
+            int price = 41500;
+            int squareFeet = 150;
+
+            //POLYMORPHISM
+            public override string[] HouseAmenities(string TinyHouseComforts)
+            {
+                return new string[]
+                {  "The entire House is light weight",
+                    "Built for sub-zero temperature",
+                    "Significant decreased environmental footprint."
+                };
+            }
+
+
+
+
+
+
+
+
+            //ABSTRACTION
+
+
+
+
+
+
+
+
+
+
+
+
+
         }
-
-
-
-
-        //POLYMORPHISM
-
-
-
-
-
-
-
-
-
-
-        //ABSTRACTION
-
-
-
-
-
-
-
-
-
-        //ENCAPSULATION
-
-
-
     }
+}
 
 
 
